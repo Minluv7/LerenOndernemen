b286c56a1890a55208b0acfa515fd642bceca9d1
Nese Yildirim <neseyild@student.arteveldehs.be>
2024-04-18 20:56:58 +0200

    install nuxt add tailwind

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="shadow-sm p-5">
+        <h1>Welkom bij Leren Ondernemen</h1>
+        <p>Op deze website vind je alle informatie die je nodig hebt om te leren ondernemen.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
