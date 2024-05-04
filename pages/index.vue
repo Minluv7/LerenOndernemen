48faf9b4d2b51c0cb88acf3f2648c5abc34b36a0
Nese Yildirim <neseyild@student.arteveldehs.be>
2024-05-04 14:38:25 +0200

    add data + get data in dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
       <a class="rounded-md bg-blue-100 border border-blue-400 text-blue-400 shadow-md flex w-[324px] px-4 py-2 justify-center items-center gap-4" href="/login">Login</a>
       
     </div>
-     <a class="rounded-lg text-white border-2 border-solid border-[#013A63] bg-[#7AA5B8] flex w-[324px] px-4 py-2 justify-center items-center gap-4" href="/register">Registreer</a>
+    
     <ul>
       <li>
        
@@ -19,6 +19,7 @@
        
       </li>
     </ul>
+     <a class="rounded-lg text-white border-2 border-solid border-[#013A63] bg-[#7AA5B8] flex w-[324px] px-4 py-2 justify-center items-center gap-4" href="/register">Registreer</a>
     <Footer />
   </div>
 </template>
