36262c59b7120906a01fa306267788e8d0e132b6
Nese Yildirim <neseyild@student.arteveldehs.be>
2024-05-09 23:10:16 +0200

    add data change font-family and font color

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+export default ({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules:  [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/cloudinary',
+  ],
+  tailwindcss: {
+    config: {
+      theme: {
+        extend: {
+          fontFamily: {
+            'lato': ['Lato', 'sans-serif'],
+          },
+        },
+      },
+    },
+  },
+
+  head: {
+    link: [
+      { rel: 'stylesheet', href: "https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" },
+    ],
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,33 @@
 <template>
-  <div >
- 
+<div>
+  <div class="headDashboard">
+
+    <h1 class=" [text-shadow:_0_2px_3px_rgb(22_40_54_/_50%)] font-inter flex-shrink-0">Education Business</h1>
+    <img src="assets/image/logos/LogoDescription.png" alt="logo description">
+ <p>Ga vandaag nog uit je comfortzone en ontdek gratis ondernemerschap.</p>
+ <p>Leer en groei!</p>
     <div class="">
-      <a class="rounded-lg text-white border-2 border-solid border-[#013A63] bg-[#7AA5B8] flex w-[324px] px-4 py-2 justify-center items-center gap-4" href="/register">Registreer</a>
-      <a class="rounded-md bg-blue-100 border border-blue-400 text-blue-400 shadow-md flex w-[324px] px-4 py-2 justify-center items-center gap-4" href="/login">Login</a>
+      <a class="rounded-lg text-white border-2 border-solid w-80 border-[#013A63] bg-[#7AA5B8] flex px-4 py-2 justify-center items-center gap-4" href="/register">Registreer</a>
+      <a class="rounded-md bg-blue-100 border border-blue-400 text-blue-400 shadow-md flex w-80 px-4 py-2 justify-center items-center gap-4" href="/login">Login</a>
       
     </div>
     
     <ul>
       <li>
-       
+      
           <div v-for=" d in dashboard" :key="d.id">
             <h2>{{d.title}}</h2>
             <p>{{d.description}}</p>
             <img :src="d.image" :alt="d.title">
             
           </div>
-       
+      
       </li>
     </ul>
      <a class="rounded-lg text-white border-2 border-solid border-[#013A63] bg-[#7AA5B8] flex w-[324px] px-4 py-2 justify-center items-center gap-4" href="/register">Registreer</a>
+    </div>
     <Footer />
+  
   </div>
 </template>
 
@@ -36,5 +43,8 @@ definePageMeta({
 </script>
 
 <style scoped>
-/* Voeg hier stijlen toe die specifiek zijn voor deze pagina */
+.headDashboard{
+  max-width: 90%;
+  margin: 0 auto;
+}
 </style>
