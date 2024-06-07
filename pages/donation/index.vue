730e45f307f14d188eadf3572b20e8b924a73019
Nese Yildirim <neseyild@student.arteveldehs.be>
2024-06-07 11:23:55 +0200

    solve error

--- pages/donation/index.vue
+++ pages/donation/index.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="space">
-        donation
+        <h1 class="text-center">Buy me a coffee</h1>
+        <div class="pt-8 flex justify-center">
+            <img width="200rem" src="assets/image/donation/buy_me_a_coffee.png" alt="coffee image">
+        </div>
+        <p class="text-center pt-8">Om mij te ondersteunen bij het maken van deze gratis inhoud en het onderhouden van mijn werk, kun je overwegen een donatie te doen via mijn Ko-fi-pagina.</p>
+        <div class="flex justify-center pt-8">
+            <a class="" href='https://ko-fi.com/W7W6W1RX4' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+        </div>
+        
     </div>
 </template>
 
@@ -14,5 +22,7 @@ definePageMeta({
 .space{
     max-width: 90%;
     margin: 0 auto;
+    padding-bottom: 6rem;
 }
+
 </style>
