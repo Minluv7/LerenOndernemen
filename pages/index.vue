9eca959b4b2e414607435045fd8f83804496c0a7
Nese Yildirim <neseyild@student.arteveldehs.be>
2024-06-10 20:23:10 +0200

    update business plan

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
-<div>
-  <Dashboard />
-  <Footer />
+  <div>
+    <Dashboard />
+    <Footer />
   </div>
 </template>
 
--- pages/jaarrekening_pb_vb/index.vue
+++ pages/jaarrekening_pb_vb/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="space">
-     <lessonsPages />
+      <lessonsPages />
     </div>
 </template>
 
