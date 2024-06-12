dcf38d95124dc6ff3144bfcd1bce850f2ab45e43
Nese Yildirim <neseyild@student.arteveldehs.be>
2024-06-12 19:55:25 +0200

    add css filter

--- components/allLessons.vue
+++ components/allLessons.vue
@@ -3,8 +3,8 @@
       <h1 class="text-center pt-8 text-wrap">{{ category.name }}</h1>
       <div class="flex justify-center">
         <ul class="grid-container pt-8 pb-20">
-          <li v-for="sub in category.subCategories" :key="sub.id" class="grid-item rounded-3xl bg-[#BADBE7]">
-            <NuxtLink :to="`/${sub.slug}`" class="flex flex-col p-4 gap-4 items-center justify-between" @click="markAsViewed(sub.id)">
+          <li v-for="sub in category.subCategories" :key="sub.id" :class="{'excluded-page': isExcluded(sub.slug)}" class="grid-item rounded-3xl bg-[#BADBE7]">
+            <NuxtLink :to="`/${sub.slug}`"  class="flex flex-col p-4 gap-4 items-center justify-between" @click="markAsViewed(sub.id)">
               <img  class="w-[8rem] h-[10rem] object-cover sub-image" :src="sub.image" :alt="sub.title" />
               <h3 >{{ sub.title }}</h3>
               <div v-if="sub.viewed" class="viewed-badge">✔️</div>
@@ -26,7 +26,7 @@ const markAsViewed = async (subCategoryId: any) => {
   // Haal de subcategorie op
   const sub = (category.value as { subCategories: any[] }).subCategories.find((sub: any) => sub.id === subCategoryId);
 
-const excludedSlugs = ["notaris", "btw_aanvraag", "sociaal_secreteriaat", "vergunning", "eenmans_vennootschap", "jaarrekening_pb_vb"];
+const excludedSlugs = ["notaris", "btw_aanvraag", "sociaal_secretariaat", "vergunning", "eenmans_vennootschap", "jaarrekening_pb_vb"];
   // Controleer of de subcategorie niet verwijst naar de exclusieve pagina's
  if (!excludedSlugs.includes(sub.slug)) {
     // Voer het postverzoek uit om de subcategorie als bekeken te markeren
@@ -40,6 +40,11 @@ const excludedSlugs = ["notaris", "btw_aanvraag", "sociaal_secreteriaat", "vergu
     // Of navigeer naar een andere pagina
     router.push('/will_come');
   }
+}
+
+const isExcluded = (slug: string) => {
+  const excludedSlugs = ["notaris", "btw_aanvraag", "sociaal_secretariaat", "vergunning", "eenmans_vennootschap", "jaarrekening_pb_vb"];
+  return excludedSlugs.includes(slug);
 }
 </script>
 
@@ -76,6 +81,10 @@ const excludedSlugs = ["notaris", "btw_aanvraag", "sociaal_secreteriaat", "vergu
   max-width: 100%;
   max-height: 100%;
   object-fit: contain;
+}
+
+.excluded-page {
+  filter: brightness(0.5); /* Donkere filter toevoegen */
 }
 
 </style>
